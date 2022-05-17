2ac68a45399c7dba665560ee10d8c7f1fb0dcfe4
momo <momosunshine@gmail.com>
2022-05-17 21:24:24 +0200

    add springboot setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Demo1';
+  message : string = "helo Angular";
+  htmlString: string ="<h1>htmlString</h1>"
+  getInfo(){
+    return "我是getInfo方法返回的内容"
+  }
+
+
+  //title =" angular-test2"
+  //imgUrl = "https://upload.wikimedia.org/wikipedia/commons/d/d7/MCM_London_2014_-_Pikachu_%2814266616051%29.jpg"
+title = "test"
+onSave(event: Event){
+  this.title
+}
+
 }
