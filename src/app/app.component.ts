ad39e708667e1311c102399c1a12056d5c460665
momo <momosunshine@gmail.com>
2022-05-17 23:08:16 +0200

    add router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+  
 })
 export class AppComponent {
-  message : string = "helo Angular";
-  htmlString: string ="<h1>htmlString</h1>"
-  getInfo(){
-    return "我是getInfo方法返回的内容"
+  
   }
 
 
-  //title =" angular-test2"
-  //imgUrl = "https://upload.wikimedia.org/wikipedia/commons/d/d7/MCM_London_2014_-_Pikachu_%2814266616051%29.jpg"
-title = "test"
-onSave(event: Event){
-  this.title
-}
 
-}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { NavigationComponent } from './pages/navigation/navigation.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+
+const routes : Routes= [
+  {
+    path: "home",
+    component:HomeComponent
+  },
+  {
+    path:"about",
+    component:AboutComponent
+  },
+  { // add the simple homenavagation
+    path:"",
+    redirectTo:"home",
+    pathMatch: "full"
+  },
+  {
+    path:'**',
+    component: NotFoundComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    LayoutComponent,
+    NavigationComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SharedModule
+    RouterModule.forRoot(routes,{useHash: true})
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
